7f50ee047679294ec5daf877c1a30c658aa6d8a5
Fran <hola@beachlab.org>
2019-12-07 19:10:47 +0100

    fix arus rotation

--- asus.sh
+++ asus.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr --output eDP-1 --primary --auto --output DVI-I-1-1 --right-of eDP-1 --auto
+xrandr --output eDP-1 --primary --auto --output DVI-I-1-1 --rotate normal --right-of eDP-1 --auto
--- asusr.sh
+++ asusr.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr --output eDP-1 --primary --auto --output DVI-I-1-1 --rotate right --right-of eDP-1 --auto
+xrandr --output eDP-1 --primary --auto --output DVI-I-1-1 --rotate left --right-of eDP-1 --auto
