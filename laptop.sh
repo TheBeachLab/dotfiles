e2813325958ea92bb1dba07b7ae4c03d67bab332
Fran <hola@beachlab.org>
2019-12-08 17:13:19 +0100

    fixed xrandr names

--- laptop.sh
+++ laptop.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr --output HDMI1 --off --output DP2 --off --output DVI-I-1-1 --off --output eDP-1 --auto
+xrandr --output HDMI-1 --off --output DP-2 --off --output DVI-I-1-1 --off --output eDP-1 --auto
