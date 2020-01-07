7ed8b71ab37b2a6a0b40870d98e43dbc744faa38
Fran <hola@beachlab.org>
2020-01-07 10:57:06 +0100

    lightworks PortAudio Device Names

--- Lightworks/setaudiodevices.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-LWDEVICE=`python -m sounddevice | grep jack | cut -c 3-4`
-
-echo "[PortAudio]" > GlobalSettings.txt
-echo "InputDeviceIndex=${LWDEVICE}" >> GlobalSettings.txt
-echo "OutputDeviceIndex=${LWDEVICE}" >> GlobalSettings.txt
-
