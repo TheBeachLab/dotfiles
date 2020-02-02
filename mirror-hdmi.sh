15f083b4f4a0410c000a783022d01779b93af7f7
Fran <hola@beachlab.org>
2020-02-02 16:35:21 +0100

    update device names

--- mirror-hdmi.sh
+++ mirror-hdmi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr --output DP2 --off --output eDP1 --primary --auto --output HDMI1 --same-as eDP1 --auto
+xrandr --output DP-2 --off --output eDP-1 --primary --auto --output HDMI-1 --same-as eDP-1 --auto
