ef07d36f5bf54a4cbbda3ebc6c6def2f31a06e64
Fran <hola@beachlab.org>
2020-07-06 18:12:09 +0200

    polybar multimonitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,8 +6,21 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar archbar &
-#polybar bar2 &
+# Launch polybar
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    if [ $m == 'DP-1' ] # center monitor
+    then
+	MONITOR=$m polybar --reload archbar-center -c ~/.config/polybar/config &
+    elif [ $m == 'DP-3' ]
+    then
+	MONITOR=$m polybar --reload archbar-left -c ~/.config/polybar/config &
+    else
+	MONITOR=$m polybar --reload archbar-right -c ~/.config/polybar/config &
+    fi
+  done
+else
+  polybar --reload archbar-center -c ~/.config/polybar/config &
+fi
 
 echo "Bars launched..."
