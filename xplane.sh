2a9795a514f04f0f98e56d864f3afbfb275c4ba6
Fran <hola@beachlab.org>
2020-07-13 10:01:11 +0200

    xrandr includes feh

--- xplane.sh
+++ xplane.sh
@@ -1,1 +1,2 @@
 xrandr --output DP-1 --primary --auto --output DP-3 --left-of DP-1 --auto --output DP-5 --right-of DP-1 --rotate normal --auto --output eDP-1-1 --below DP-1 --auto
+feh --bg-scale ~/Pictures/wallpapers/w1.png --bg-scale ~/Pictures/wallpapers/w2.jpg --bg-scale ~/Pictures/wallpapers/w3.jpg --bg-scale ~/scripts/spacesurf.png
