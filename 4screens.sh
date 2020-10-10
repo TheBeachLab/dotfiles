b727a5a44424194289861658041a3fd3d977e05b
Fran <hola@beachlab.org>
2020-10-10 14:02:08 +0200

    rename 4 screens script

--- 4screens.sh
+++ 4screens.sh
@@ -1,2 +1,7 @@
-xrandr --output DP-1 --primary --auto --output DP-3 --left-of DP-1 --auto --output DP-5 --right-of DP-1 --rotate left --auto --output eDP-1-1 --below DP-1 --auto
-feh --bg-scale ~/Pictures/wallpapers/w1.png --bg-scale ~/Pictures/wallpapers/w2.jpg --bg-scale ~/Pictures/wallpapers/w32.jpg --bg-scale ~/scripts/spacesurf.png
+# setup 4 screens
+xrandr --output DP-1 --primary --auto --output DP-3 --left-of DP-1 --auto --output DP-5 --right-of DP-1 --rotate normal --auto --output eDP-1-1 --below DP-1 --auto
+# set wallpapers
+feh --bg-scale ~/Pictures/wallpapers/w1.png --bg-scale ~/Pictures/wallpapers/w2.jpg --bg-scale ~/Pictures/wallpapers/w3.jpg --bg-scale ~/scripts/spacesurf.png
+# reload polybar
+sh ~/.config/polybar/launch.sh
+
