742213e6a81d84b12003df0c4155ddd8808fa0bf
Fran <hola@beachlab.org>
2020-10-11 11:53:48 +0200

    let polybar set the wallpaper

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-read -t 2 -p "waiting for initial xrand config\n"
+read -t 0.2 -p "waiting for initial xrand config\n"
 
 # Launch polybars
 if type "xrandr"; then
@@ -16,8 +16,10 @@ if type "xrandr"; then
         # Things to do when egpu is connected
 	xrandr --output DP-1 --primary --auto --output DP-3 --left-of DP-1 --auto --output DP-5 --right-of DP-1 --rotate normal --auto --output eDP-1-1 --below DP-1 --auto # setup 4 screens
         # set wallpapers
+	feh --bg-scale ~/scripts/spacesurf.png
         #feh --bg-scale ~/Pictures/wallpapers/w1.png --bg-scale ~/Pictures/wallpapers/w2.jpg --bg-scale ~/Pictures/wallpapers/w3.jpg --bg-scale ~/scripts/spacesurf.png
-	read -t 2 -p "waiting for new xrand to settle\n"
+	#read -t 0.2 -p "waiting for new xrand to settle\n"
+	# set polybars
         for k in $(xrandr --query | grep " connected" | cut -d" " -f1); do  # loop again connected screens
           if [ $k == 'eDP-1-1' ]  # laptop screen
           then
@@ -32,6 +34,9 @@ if type "xrandr"; then
     elif [ $m == 'eDP-1' ]  # no egpu connected
     then
         # Things to do when laptop alone
+        # set wallpaper
+        feh --bg-scale ~/scripts/spacesurf.png
+        # set polybar
 	MONITOR=$m polybar --reload archbar-laptop -c ~/.config/polybar/config &
     fi
   done
