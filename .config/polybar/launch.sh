b4ace4030d447fbda74bb133e70502e05a4d1258
Fran <hola@beachlab.org>
2020-10-11 11:26:45 +0200

    wait for xrandr to settle before changing screens

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,6 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+read -t 2 -p "waiting for initial xrand config\n"
+
 # Launch polybars
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do  # check all connected screens
@@ -13,6 +15,9 @@ if type "xrandr"; then
     then
         # Things to do when egpu is connected
 	xrandr --output DP-1 --primary --auto --output DP-3 --left-of DP-1 --auto --output DP-5 --right-of DP-1 --rotate normal --auto --output eDP-1-1 --below DP-1 --auto # setup 4 screens
+        # set wallpapers
+        #feh --bg-scale ~/Pictures/wallpapers/w1.png --bg-scale ~/Pictures/wallpapers/w2.jpg --bg-scale ~/Pictures/wallpapers/w3.jpg --bg-scale ~/scripts/spacesurf.png
+	read -t 2 -p "waiting for new xrand to settle\n"
         for k in $(xrandr --query | grep " connected" | cut -d" " -f1); do  # loop again connected screens
           if [ $k == 'eDP-1-1' ]  # laptop screen
           then
